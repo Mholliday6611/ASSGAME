85a582550c9dc26900aa3648e4f4630de40bfde9
Ripmma <Ripmma@gmail.com>
2017-07-18 18:07:27 -0400

    Update

---
+++ public/controllers/tempApp.js
@@ -0,0 +1,23 @@
+var myApp = angular.module("gameApp", ["ngRoute"])
+	.config(function($routeProvider){
+		$routeProvider
+		.when("/", {
+			templateUrl: "directory.html"
+		})
+		.when("/forum-post", {
+			templateUrl: "forum-post.html"
+		})
+	});
+
+myApp.controller('gameCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.post = function() {
+		console.log($scope.list);
+		$http.post("/questionList", $scope.list).then(function(response) {
+			console.log(response.data);
+
+		});
+	}
+	$scope.search = function() {
+		console.log("Aloha Loser");
+	}
+}]);
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express"),
 	bodyParser = require("body-parser"),
+    mongojs = require("mongojs"),
 	app = express();
 
-app.use(express.static(__dirname, + "/public"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.post("/questionList", function(req, res){
+	console.log(req.body);
+
+});
 
 app.listen(1000);
 console.log("Server running on port 1000");
